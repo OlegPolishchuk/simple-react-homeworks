0e4e453cce1228ed9cb1e71049558e899a4d7fae
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-03-30 21:26:55 +0300

    feat: add animated toggle

--- src/p2-homeworks/h4/common/ToggleBtn/ToggleBtn.tsx
+++ src/p2-homeworks/h4/common/ToggleBtn/ToggleBtn.tsx
@@ -1,49 +1,24 @@
-import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, useState} from 'react';
+import React, {useState} from 'react';
 import s from "./ToggleBtn.module.css";
 
-// type ToggleBtnPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 type ToggleBtnPropsType = {
-    onChange: () => void
+    toggle: () => void
 }
-export const ToggleBtn: React.FC<ToggleBtnPropsType> = (
-    {
-        // type,
-        onChange
-    }
-) => {
 
-    // const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    //     onChange && onChange(e)
-    // }
-    const [toggle, setToggle] = useState<boolean>(false)
+export const ToggleBtn: React.FC<ToggleBtnPropsType> = ({toggle}) => {
+
+    const [wrapped, setWrapped] = useState<boolean>(false)
 
     const onClickHandler = () => {
-        setToggle(toggle => !toggle)
+        setWrapped(wrapped => !wrapped)
+        toggle()
     }
 
     return (
-        // <div className={s.toggle_btn} >
-        //     <input
-        //         className={s.checkbox}
-        //         type={"checkbox"}
-        //         onChange={onChangeHandler}
-        //     />
-        //     <div className={s.hamburger_lines}>
-        //             <span className={`${s.line} ${s.line1}`}>
-        //
-        //             </span>
-        //         <span className={`${s.line} ${s.line2}`}>
-        //
-        //             </span>
-        //         <span className={`${s.line} ${s.line3}`}>
-        //
-        //             </span>
-        //     </div>
-        // </div>
         <div className={s.menu_bar} onClick={onClickHandler}>
-            <div id="bar1" className={`${s.bar} ${s.bar1} ${toggle ? s.change : ''}`}> </div>
-            <div id="bar2" className={`${s.bar} ${s.bar2} ${toggle ? s.change : ''}`}> </div>
-            <div id="bar3" className={`${s.bar} ${s.bar3} ${toggle ? s.change : ''}`}> </div>
+            <div className={`${s.bar} ${s.bar1} ${wrapped ? s.change : ''}`}> </div>
+            <div className={`${s.bar} ${s.bar2} ${wrapped ? s.change : ''}`}> </div>
+            <div className={`${s.bar} ${s.bar3} ${wrapped ? s.change : ''}`}> </div>
         </div>
     );
 };
--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react'
+import React, {useState} from 'react'
 import {NavLink} from 'react-router-dom'
 import s from './Header.module.css'
 import {ToggleBtn} from "../h4/common/ToggleBtn/ToggleBtn";
@@ -7,17 +7,14 @@ function Header() {
 
     const [toggle, setToggle] = useState<boolean>(false)
     const classNameLinksBox = toggle? s.show_links : '';
-    // const toggleLinks = (e: ChangeEvent<HTMLInputElement>) => {
-    //     // setToggle(e.currentTarget.checked)
-    // }
-    const toggleLinks = () => {
 
+    const toggleLinks = () => {
         setToggle(toggle => !toggle)
     }
 
     return (
         <nav className={s.header_nav}>
-            <div className={`${s.nav_wrapper} ${classNameLinksBox}`}>
+            <div className={`${s.nav_wrapper} ${classNameLinksBox} ${toggle ? s.show_nav : ''}`}>
                 <NavLink
                     to={'/pre-junior'}
                     className={({isActive}) => isActive ? s.active : ''}
@@ -36,7 +33,7 @@ function Header() {
                 >
                     <h3>Junior+</h3>
                 </NavLink>
-                <ToggleBtn  onChange={toggleLinks}/>
+                <ToggleBtn  toggle={toggleLinks}/>
             </div>
         </nav>
     )
