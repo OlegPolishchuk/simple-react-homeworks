e8ca744d14a1a599850bbe0ab76323eeba217c32
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-04-06 20:52:25 +0300

    fix: HW6. component style

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import s from './../h4/common/c2-SuperButton/SuperButton.module.css'
+import './../../App.css'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
@@ -15,22 +16,25 @@ function HW6() {
         setValue(state)
     }
 
+
     return (
         <div>
             <hr/>
             homeworks 6
 
             {/*should work (должно работать)*/}
-            <div >
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
-                />
-            </div>
-            <SuperButton className={`${s.default} ${s.white}`} onClick={save}>save</SuperButton>
-            <SuperButton className={`${s.default} ${s.white}`} onClick={restore}>restore</SuperButton>
+            <div className={'container'}>
+                <div>
+                    <SuperEditableSpan
+                        value={value}
+                        onChangeText={setValue}
+                        spanProps={{children: value ? undefined : 'enter text...'}}
+                    />
+                </div>
+                <SuperButton className={`${s.default} ${s.white}`} onClick={save}>save</SuperButton>
+                <SuperButton className={`${s.default} ${s.white}`} onClick={restore}>restore</SuperButton>
 
+            </div>
         </div>
     )
 }
